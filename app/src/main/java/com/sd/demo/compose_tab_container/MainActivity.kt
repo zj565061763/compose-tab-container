d9404235ca1c046b14cbc37c23e491fbbff7625f
zj565061763 <565061763@qq.com>
2023-06-15 14:57:22 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_tab_container/MainActivity.kt
+++ app/src/main/java/com/sd/demo/compose_tab_container/MainActivity.kt
@@ -6,14 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sd.demo.compose_tab_container.ui.theme.AppTheme
+import com.sd.lib.compose.tab.container.TabContainer
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,25 +37,66 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Content() {
+
+    val tabs = remember { listOf("A", "B") }
+    var selectedTabIndex by remember { mutableStateOf(0) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(5.dp)
     ) {
-        Button(
-            onClick = {
-
-            }
+        TabRow(
+            modifier = Modifier.fillMaxWidth(),
+            selectedTabIndex = selectedTabIndex,
         ) {
-            Text(text = "button")
+            tabs.forEachIndexed { index, item ->
+                val selected = index == selectedTabIndex
+                Tab(
+                    modifier = Modifier.height(50.dp),
+                    selected = selected,
+                    onClick = { selectedTabIndex = index },
+                ) {
+                    Text(text = item)
+                }
+            }
+        }
+
+        TabContainer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            key = tabs[selectedTabIndex],
+        ) {
+            tab("A") {
+                TabContent(tag = "I am A")
+            }
+            tab("B") {
+                TabContent(tag = "I am B")
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-private fun DefaultPreview() {
-    AppTheme {
-        Content()
+private fun TabContent(
+    modifier: Modifier = Modifier,
+    tag: String,
+) {
+    var count by remember { mutableStateOf(0) }
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(1000)
+            count++
+        }
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = tag)
+        Text(text = count.toString())
     }
 }
