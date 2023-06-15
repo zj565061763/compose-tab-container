74bef38a2710901727059dad68449f1399d278e1
zj565061763 <565061763@qq.com>
2023-06-15 14:41:00 +0800

    foundation

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
 }
 
 afterEvaluate {
