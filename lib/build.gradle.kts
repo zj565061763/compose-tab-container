e3b504e54620549d0829519057b19b60678c3984
zjun <565061763@qq.com>
2024-12-28 21:07:15 +0800

    Revert "1.5.0"
    This reverts commit ccb363661d7dcb5e704324aae8686511bef1ba42.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-tab-container"
-val libVersion = "1.5.0"
+val libVersion = "1.1.0"
 
 android {
   namespace = "com.sd.lib.compose.tab.container"
